32ef35f90119d2a4c7ec7ec97e178e2bb6cf92d4
Karen Croston <karencroston@hotmail.co.uk>
2023-10-26 08:27:58 +0100

    create accordian component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
-import Button from "./Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Button secondary outline rounded className="mb-5">
-          <GoBell />
-          Click me!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoCloud />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: "1114sss",
+      label: "Can I use React on a project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want",
+    },
+    {
+      id: ";dlii,kd2",
+      label: "Can I use Javascript on a project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want",
+    },
+    {
+      id: "kkdl776",
+      label: "Can I use CSS on a project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want",
+    },
+  ];
+
+  return <Accordion items={items} />;
 }
 
 export default App;
